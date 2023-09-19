5451d8d3af93fccdd4ce13437d59086d686363ed
Blue Jessen <canealisblue@gmail.com>
2023-09-19 16:21:30 -0500

    Add icons to nav bar and add routing to recipe form page

--- src/app/components/navBar/navBar.js
+++ src/app/components/navBar/navBar.js
@@ -1,4 +1,5 @@
 import styles from './navBar.module.scss'
+import Link from 'next/link'
 const NavBar = () => {
 return(
     <header className={styles.navHeader}>
@@ -6,8 +7,13 @@ return(
              <h1>Jessen Family Cookbook</h1>
         </div>
         <div className={styles.topNavRight}>
-            <button className={styles.navButton}>New Recipe</button>
-            <button className={styles.navButton}>NavMenu</button>
+            <Link href='new-recipe' className={styles.newRecipeButton}>
+              <img src='New-Recipe-Icon.png' alt='New Recipe Button'/>
+            </Link>
+           
+            <button className={styles.menuBurgerButton}>
+             <img src='menu-burger.png' alt='Open Side Menu'/>
+            </button>
         </div>
     </header>
 )
---
+++ src/app/new-recipe/page.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/navBar/navBar";
+
+export default function RecipeFormPage() {
+
+    return (
+      <main >
+        <NavBar/>
+        <h1>Recipe Form Here</h1>
+      </main>
+    )
+  
+  }

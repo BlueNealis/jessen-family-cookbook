3a5848cd55cc561a4964a9c0d83625fa8b5672b3
Blue Jessen <canealisblue@gmail.com>
2023-09-19 11:35:16 -0500

    Add hardcoded recipe pageand basic html layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 import NavBar from './components/navBar/navBar.js'
 import RecipeThumbnail from './components/recipeThumbnail/recipeThumbnail'
@@ -9,7 +10,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
      <NavBar/>
-     <RecipeThumbnail/>
+     <div>
+     <Link href='recipe-card'>
+        <RecipeThumbnail/>
+      </Link>
+     </div>
     </main>
   )
 
---
+++ src/app/recipe-card/page.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/navBar/navBar"
+import RecipeCard from "./recipeCard"
+
+export default function RecipePage() {
+
+    return (
+      <main >
+        <NavBar/>
+        <RecipeCard/>
+
+      </main>
+    )
+  
+  }

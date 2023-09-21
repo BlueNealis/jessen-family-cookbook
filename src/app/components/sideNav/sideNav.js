41179fafc5843f7cf85af1bac7f8184c7e7e279a
Blue Jessen <canealisblue@gmail.com>
2023-09-20 23:19:50 -0500

    Add close button to side nav menu

--- src/app/components/sideNav/sideNav.js
+++ src/app/components/sideNav/sideNav.js
@@ -13,21 +13,23 @@ const SideNav = ({visible}) => {
        } else {
         setNavClasses(`${styles.sideNavContainer}`)
        }
+    },[visible])
 
-    })
-
-
+const handleClick = (e) => {
+    setNavClasses(`${styles.sideNavContainer}`)
+}
 
     return(
         <div className={navClasses}>
+        <button onClick={handleClick} className={styles.closeNavButton}>X</button>
             <h1>Menu</h1>
-            <Link href=''>
+            <Link href='/'>
                 <h2>Home</h2>
             </Link>
             <Link href='all-recipes'>
                 <h2>All Recipes</h2>
             </Link>
-            <Link href='add-a-recipe'>
+            <Link href='new-recipe'>
                 <h2>Add A Recipe</h2>
             </Link>
         </div>
